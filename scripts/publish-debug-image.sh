da2dafb5e06c8ea9a9b2b45456bbb485cbb9e189
Gannicus <gannicus@orbitus.ca>
2024-03-17 01:25:26 -0400

    update all files again

--- scripts/publish-debug-image.sh
+++ scripts/publish-debug-image.sh
@@ -12,8 +12,8 @@ echo "debug = true" >> Cargo.toml
 
 TAG=$1-debug
 echo "Building images, will tag for ghcr.io with $TAG-debug!"
-docker build -t ghcr.io/revoltchat/base:latest -f Dockerfile.useCurrentArch .
-docker build -t ghcr.io/revoltchat/server:$TAG - < crates/delta/Dockerfile
-docker build -t ghcr.io/revoltchat/bonfire:$TAG - < crates/bonfire/Dockerfile
+docker build -t ghcr.io/onechatsocial/base:latest -f Dockerfile.useCurrentArch .
+docker build -t ghcr.io/onechatsocial/server:$TAG - < crates/delta/Dockerfile
+docker build -t ghcr.io/onechatsocial/bonfire:$TAG - < crates/bonfire/Dockerfile
 
 git restore Cargo.toml
