5ec73d317f1dde943527a04623e9f65f201a6d2d
Gannicus <gannicus@orbitus.ca>
2024-03-26 22:24:53 -0400

    Changes for API permissions set

--- crates/delta/src/routes/channels/permissions_set.rs
+++ crates/delta/src/routes/channels/permissions_set.rs
@@ -28,7 +28,7 @@ pub async fn req(
     data: Json<Data>,
 ) -> Result<Json<Channel>> {
     // Code from default
-    let mut channel = match target.as_channel(db).await {
+    let channel = match target.as_channel(db).await {
         Ok(c) => c,
         Err(e) => {
             eprintln!("Error Code 1: {:?}", e);
@@ -46,82 +46,56 @@ pub async fn req(
         }
     }
 
-    match &channel {
-        Channel::TextChannel { .. } | Channel::VoiceChannel { .. } => {
-            let server = match perm.server.get().ok_or(Error::InvalidOperation) {
-                Ok(s) => s,
-                Err(e) => {
-                    eprintln!("Error Code 3: {:?}", e);
-                    return Err(e);
+    let mut channel_clone = channel.clone();
+    let perm_clone = perm.clone();
+    if let Some(channel) = perm_clone.channel.get() {
+        match channel {
+            Channel::TextChannel { server: server_id, .. } | Channel::VoiceChannel { server: server_id, .. } => {
+                let server = match Ref::from_unchecked(server_id.to_string()).as_server(db).await {
+                    Ok(s) => s,
+                    Err(e) => {
+                        eprintln!("Error Code 3: {:?}", e);
+                        return Err(e);
+                    }
+                };
+
+                let role = match server.roles.get(&role_id).ok_or(Error::NotFound) {
+                    Ok(r) => r,
+                    Err(e) => {
+                        eprintln!("Error Code 4: {:?}", e);
+                        return Err(e);
+                    }
+                };
+    
+                //  We are not an elevated user
+                if role.rank <= perm.get_member_rank().unwrap_or(i64::MIN) {
+                    return Err(Error::NotElevated);
                 }
-            };
-
-            let role = match server.roles.get(&role_id).ok_or(Error::NotFound) {
-                Ok(r) => r,
-                Err(e) => {
-                    eprintln!("Error Code 4: {:?}", e);
-                    return Err(e);
+    
+                let current_value: Override = role.permissions.into();
+    
+                match perm.throw_permission_override(db, current_value, data.permissions).await {
+                    Ok(_) => {},
+                    Err(e) => {
+                        eprintln!("Error Code 5: {:?}", e);
+                        return Err(e);
+                    }
                 }
-            };
-
-            //  We are not an elevated user
-            if role.rank <= perm.get_member_rank().unwrap_or(i64::MIN) {
-                return Err(Error::NotElevated);
-            }
-
-            let current_value: Override = role.permissions.into();
-
-            match perm.throw_permission_override(db, current_value, data.permissions).await {
-                Ok(_) => {},
-                Err(e) => {
-                    eprintln!("Error Code 5: {:?}", e);
-                    return Err(e);
+    
+                match channel_clone.set_role_permission(db, &role_id, data.permissions.into()).await {
+                    Ok(_) => {},
+                    Err(e) => {
+                        eprintln!("Error Code 6: {:?}", e);
+                        return Err(e);
+                    }
                 }
+    
+                Ok(Json(channel_clone))
+
             }
-
-            match channel.set_role_permission(db, &role_id, data.permissions.into()).await {
-                Ok(_) => {},
-                Err(e) => {
-                    eprintln!("Error Code 6: {:?}", e);
-                    return Err(e);
-                }
-            }
-
-            Ok(Json(channel))
-        }
-        _ => return Err(Error::InvalidOperation),
-    }
-
-    // original code
-    /*
-    let mut channel = target.as_channel(db).await?;
-    let mut permissions = perms(&user).channel(&channel);
-
-    permissions
-        .throw_permission_and_view_channel(db, Permission::ManagePermissions)
-        .await?;
-
-    if let Some(server) = permissions.server.get() {
-        if let Some(role) = server.roles.get(&role_id) {
-            if role.rank <= permissions.get_member_rank().unwrap_or(i64::MIN) {
-                return Err(Error::NotElevated);
-            }
-
-            let current_value: Override = role.permissions.into();
-            permissions
-                .throw_permission_override(db, current_value, data.permissions)
-                .await?;
-
-            channel
-                .set_role_permission(db, &role_id, data.permissions.into())
-                .await?;
-
-            Ok(Json(channel))
-        } else {
-            Err(Error::NotFound)
+            _ => return Err(Error::InvalidOperation),
         }
     } else {
         Err(Error::InvalidOperation)
     }
-    */
 }
